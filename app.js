25add865abf9c44766c59b14c0784e8d85f28c67
redpolarbear <matthewxu@outlook.com>
2017-05-07 22:05:03 -0700

    index pages

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ App({
     } else {
       wx.login({
         success: function (res) {
-          // console.log(res)
           var js_code = res.code
           var appid = 'wxf2868d0e8c61d87f'
           var secret = '563a4e932c72054155b44fc11e54e202'
@@ -28,45 +27,14 @@ App({
           wx.request({
             url: 'https://api.weixin.qq.com/sns/jscode2session',
             data: { appid, secret, js_code, grant_type },
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            // header: {}, // 设置请求的 header
+            method: 'GET', 
             success: function (res) {
               // success
-              // console.log(res)
-              var session_key = res.data.session_key // get the session_key
+              var openid = res.data.openid // get the unique openid for each login user
               wx.getUserInfo({
                 success: function (res) {
-                  // console.log(res)
-                  var encryptedData = res.encryptedData
-                  var iv = res.iv
                   that.globalData.userInfo = res.userInfo
-                  // wx.request({
-                  //   url: 'http://localhost:3000/onLogin',
-                  //   data: {
-                  //     session_key,
-                  //     iv,
-                  //     encryptedData,
-                  //   },
-                  //   method: 'post', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-                  //   // header: {}, // 设置请求的 header
-                  //   success: function(res){
-                  //     // success
-                  //     // console.log(res)
-                  //     that.globalData.userUid = res.data.openId
-                  //     wx.setStorage({
-                  //       key: 'openId',
-                  //       data: res.data.openId,
-                  //       success: function(res){
-                  //         // success
-                  //       },
-                  //       fail: function(res) {
-                  //         // fail
-                  //       },
-                  //       complete: function(res) {
-                  //         // complete
-                  //       }
-                  //     })
-                  that.globalData.userUid = 'oCU8L0Z-SrStLT6QC62oSUgyVw3M'
+                  that.globalData.userUid = openid
                   typeof cb == "function" && cb(that.globalData.userInfo, that.globalData.userUid)
                 },
                 fail: function (res) {
--- pages/bookinfo/bookinfo.js
+++ pages/bookinfo/bookinfo.js
@@ -45,7 +45,7 @@ Page({
         } else if (snap.val() == null) {
           // search the book in JUHE by the isbn
           wx.request({
-            url: 'https://feedback.api.juhe.cn/ISBN',
+            url: 'http://feedback.api.juhe.cn/ISBN',
             data: {
               key: '436d9b993fd3c3138954fd6fc9f89053',
               sub: param
